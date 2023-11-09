ba8a4b40b86c15804672c17466c39c259e3e9b34
Maheshvema <babumahesh2088@gmail.com>
2023-11-09 15:06:44 +0530

    modified

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Display The First Ten Natural Numbers: using for loop");
+            //for(int i=1; i<=10;i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Display The First Ten Natural Numbers:Using While Loop");
+            //int i = 1;
+            //while(i<=10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //Console.WriteLine("Display The First Ten Natural Numbers:Using Do-While Loop");
+            //int i = 1;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //while (i <= 10);
+            //Console.ReadKey();
+
+        }
+    }
+}
